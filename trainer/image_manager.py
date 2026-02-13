11b646c804bee106dc0d545901a454eb7f8c36e2
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2026-02-13 08:54:05 -0800

    no cache on miner image build

--- trainer/image_manager.py
+++ trainer/image_manager.py
@@ -64,7 +64,7 @@ def build_docker_image(
     context_path: str = ".",
     is_image_task: bool = False,
     tag: str = None,
-    no_cache: bool = False,
+    no_cache: bool = True,
 ) -> tuple[str, str | None]:
     client: docker.DockerClient = docker.from_env()
 
