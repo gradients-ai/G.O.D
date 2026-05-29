230c1b4770677b6b595f102efc82f85be486daf6
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2026-05-29 16:21:25 -0700

    update pvp-evaluator image

--- core/constants.py
+++ core/constants.py
@@ -22,7 +22,7 @@ VALIDATOR_DOCKER_IMAGE = "gradientsio/text-evaluator:basilica"
 VALIDATOR_DOCKER_IMAGE_DIFFUSION = "gradientsio/image-evaluator:basilica"
 VALIDATOR_DOCKER_IMAGE_ENV = "gradientsio/env-evaluator:basilica"
 VALIDATOR_DOCKER_IMAGE_INTERCODE = "phoenixbeaudry/env-eval-intercode:basilica"
-VALIDATOR_DOCKER_IMAGE_PVP = "phoenixbeaudry/pvp-evaluator:v7"
+VALIDATOR_DOCKER_IMAGE_PVP = "phoenixbeaudry/pvp-evaluator:v8"
 MCTS_API_DOCKER_IMAGE = "diagonalge/mcts-api:latest"
 
 
